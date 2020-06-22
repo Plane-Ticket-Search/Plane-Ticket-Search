05b1f5ed11afa494614039108bf490b37762dafc
ayame-selachan <57983388+ayame-selachan@users.noreply.github.com>
2020-06-22 11:02:58 +0800

    622 sh

---
+++ search.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <conio.h>
+#include <malloc.h>
+#include <time.h>
+#include <errno.h>
+#include <memory.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+#include "./Hash-search/hash-search.c"
+#include "./Bplus-search/bp-search.c"
+#include "./B-search/b-search.c"
